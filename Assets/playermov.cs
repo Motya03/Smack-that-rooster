52cce30706a21fcc247c91b07d618055c14de3f9
Santiago Cardozo <114632088+scardozoprogramming@users.noreply.github.com>
2025-09-26 13:54:28 +0200

    Movimineto y salto jugador con los Inputs

--- Assets/playermov.cs
+++ Assets/playermov.cs
@@ -7,6 +7,7 @@ public class playermov : MonoBehaviour
     private float movementX;
     private float movementY;
     public float speed = 1;
+    public float jumpForce = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -27,4 +28,20 @@ public class playermov : MonoBehaviour
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
+
+    private void OnJump(InputValue value)
+    {
+        // Solo saltar si se presiona el botón y está en el suelo
+        if (value.isPressed && IsGrounded())
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        // Usamos un raycast hacia abajo para detectar si estamos tocando el suelo
+        return Physics.Raycast(transform.position, Vector3.down, 1.1f);
+    }
+
 }
