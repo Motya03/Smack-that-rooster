96a7854b9927931393618d7435cc48e4c7c5fdee
Unai Arias <unaiariaslaborda@gmail.com>
2025-10-01 00:53:51 +0200

    Primer intento Local mulptiplayer
    He conseguido que primero se instancie un player y luego otro, además de que uno vaya con teclado y otro con mando, pero siempre me instancia el mismo Player. Tengo que intentar que me instancie diferentes players

---
+++ Assets/Unai/PlayerSpawn.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerSpawn : MonoBehaviour
+{
+
+    public Transform[] SpawnPoints;
+    private int m_playerCount;
+    
+    public void OnPlayerJoined(PlayerInput playerInput)
+    {
+        playerInput.transform.position = SpawnPoints[m_playerCount].transform.position;
+        /*
+        if (m_playerCount == 0)
+        {
+            playerInput.GetComponent<PlayerController>().SwitchOnCamera();
+        }
+        */
+        m_playerCount++;
+    }
+}
