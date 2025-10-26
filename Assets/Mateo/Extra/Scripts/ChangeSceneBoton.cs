c0e3744d8f5c43f2e258165d0fa8a05045f8a585
Motya03 <164553217+Motya03@users.noreply.github.com>
2025-10-26 21:56:34 +0100

    menu

---
+++ Assets/Mateo/Extra/Scripts/ChangeSceneBoton.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ChangeSceneBoton : MonoBehaviour
+{
+    public string nombreEscena;
+    public float delay = 1.5f;
+
+    public Animator imagenAnimator1; // arrastra tu primer Animator aquí desde el Inspector
+    public Animator imagenAnimator2; // arrastra el segundo Animator
+
+    public void CambioEscena()
+    {
+        GameData.ResetGameState();
+
+        // Aquí es donde activas el estado "Move" en ambos animators
+        if (imagenAnimator1 != null)
+        {
+            imagenAnimator1.Play("Move");
+        }
+
+        if (imagenAnimator2 != null)
+        {
+            imagenAnimator2.Play("Move");
+        }
+
+        StartCoroutine(EsperarYEjecutar());
+    }
+
+    IEnumerator EsperarYEjecutar()
+    {
+        yield return new WaitForSeconds(delay);
+
+        if (string.IsNullOrEmpty(nombreEscena))
+        {
+            SalirDelJuego();
+        }
+        else
+        {
+            SceneManager.LoadScene(nombreEscena);
+        }
+    }
+
+    void SalirDelJuego()
+    {
+        Application.Quit();
+    }
+}
