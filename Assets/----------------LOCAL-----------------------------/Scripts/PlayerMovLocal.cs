b312d9ff671c7915ce34beacd808384305c60976
Motya03 <164553217+Motya03@users.noreply.github.com>
2025-11-04 14:46:23 +0100

    patada

--- Assets/----------------LOCAL-----------------------------/Scripts/PlayerMovLocal.cs
+++ Assets/----------------LOCAL-----------------------------/Scripts/PlayerMovLocal.cs
@@ -13,6 +13,7 @@ public class PlayerMovLocal : MonoBehaviour
     private Animator myAnimator;
 
     private GameObject currentStunEffect;
+   public GameObject PatadaEffectPrefab;
 
     private Vector2 lastMoveInput;
     private Vector2 moveInput;
@@ -29,6 +30,7 @@ public class PlayerMovLocal : MonoBehaviour
     public Text contadorVida;
     public GameObject kickWindPrefab;
     public Transform footTrigger;
+    //public Transform atttackPoint;
 
     [Header("Movement Settings")]
     public float moveSpeed = 5f;
@@ -302,12 +304,19 @@ public class PlayerMovLocal : MonoBehaviour
 
     private void AttackPatada()
     {
-        myAnimator.Play("AttackPatada");
+        PatadaEffectPrefab.transform.rotation = Quaternion.Euler(90, 0, 0); // ejemplo: rotar 90° en Y
+        PatadaEffectPrefab.transform.rotation = transform.rotation;
+
+        Vector3 spawnPos = transform.position;
+            PatadaEffectPrefab = Instantiate(PatadaEffectPrefab, spawnPos, transform.rotation);
+            PatadaEffectPrefab.transform.localRotation = Quaternion.identity;
+            Debug.Log("Patada");
+
+        
+            myAnimator.Play("AttackPatada");
         StopMove();
        // SpawnKickFX();
         SetState(States.Idle);
-
-    
     }
 
     private void AttackLow()
